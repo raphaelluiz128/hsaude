6fd5626f99cb26cdbb9a484b776e28c9e3759081
unknown <raphaelluiz128@hotmail.com>
2019-02-12 14:57:34 -0200

    adaptação de novo nome de projeto

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -2,7 +2,7 @@ import Reactotron from 'reactotron-react-native';
 
 if (__DEV__) {
   const tron = Reactotron.configure({
-    name: 'Healthy',
+    name: 'hsaude',
   }) // controls connection & communication settings
     .useReactNative() // add all built-in react native plugins
     .connect(); // let's connect!
