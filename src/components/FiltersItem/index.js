e96f5965c41d82e87b8b7e7553957f5007c33261
RAPHAEL LUIZ SCHETTINO <raphaelluiz128@hotmail.com>
2019-02-12 14:00:37 -0200

    update filters effects

--- src/components/FiltersItem/index.js
+++ src/components/FiltersItem/index.js
@@ -1,30 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { Image, TouchableOpacity, View } from 'react-native';
 
 import LinearGradient from 'react-native-linear-gradient';
 import Icon from 'react-native-vector-icons/FontAwesome5';
+import Filters from '~/components/Filters';
 
 import styles from './styles';
 
 
-filterEx = () => {
-  console.warn('acessou');
+export default class FiltersItem extends Component {
+  
+  state = {
+    active : this.props.active,
+  };
+
+  filterEx = () => {
+
+    if (this.state.active == true) {
+      this.setState({ active: false });
+    } else {
+      this.setState({ active: true });
+    }
+  };
+
+  render() {
+    const { imageUrl } = this.props;
+    const { active } = this.state;
+    return (
+      <TouchableOpacity onPress={this.filterEx}>
+        {active && (
+          <View style={styles.badge}>
+            <Icon
+              name="check"
+              size={10}
+              color="#fff"
+            />
+          </View>
+        )}
+        <LinearGradient
+          colors={['#7F38F4', '#FD3C29']}
+          style={styles.container}
+        >
+          {console.tron.log(imageUrl)}
+          <Image source={imageUrl} />
+        </LinearGradient>
+      </TouchableOpacity>
+    );
+  }
 }
-const FiltersItem = ({ imageUrl, active }) => (
-  <TouchableOpacity onPress={this.filterEx}>
-    {active && (
-      <View style={styles.badge}>
-        <Icon name="check" size={10} color="#fff" />
-      </View>
-    )}
-    <LinearGradient colors={['#7F38F4', '#FD3C29']} style={styles.container}>
-      {console.tron.log(imageUrl)}
-      <Image source={imageUrl} />
-    </LinearGradient>
-  </TouchableOpacity>
-);
 
 
 FiltersItem.defaultProps = {
@@ -32,9 +57,8 @@ FiltersItem.defaultProps = {
   imageUrl: '',
 };
 
+
 FiltersItem.propTypes = {
   imageUrl: PropTypes.number,
   active: PropTypes.bool,
 };
-
-export default FiltersItem;
